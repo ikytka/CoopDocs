03a77f459860a3d4506b704db9d512160bf50213
Petr Sahula <spetex@icloud.com>
2015-10-13 13:45:00 +0200

    Hotfix - readability

--- docs-updater.sh
+++ docs-updater.sh
@@ -1,6 +1,6 @@
 cd /webapps/CoopDocs/pages/docs/
 git pull && echo "Pulled new version from the repo successful."
-./indexgen.sh && echo "Running Indexgen script..."
+./indexgen.sh && echo "Indexgen script finished building CoopDocs Index File."
 cd /webapps/CoopDocs/
 echo "Trying to build CoopDocs."
 gatsby build && echo "Gatsby has builded your documentation. Rejoice!"
