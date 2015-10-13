001a89d24368ae62e6b42a63a1aa55be2d64d2f7
Petr Sahula <spetex@icloud.com>
2015-10-13 12:41:39 +0200

    Add some verbose to docs-updater
    This is just for sake of your mind, docs-updater
    takes 15 minutes to complete on Raspberry Machines.

--- docs-updater.sh
+++ docs-updater.sh
@@ -1,5 +1,6 @@
 cd /webapps/CoopDocs/pages/docs/
-git pull
-./indexgen.sh
+git pull && echo "Pulled new version from the repo successful."
+./indexgen.sh && echo "Running Indexgen script..."
 cd /webapps/CoopDocs/
+echo "Trying to build CoopDocs."
 gatsby build && echo "Gatsby has builded your documentation. Rejoice!"
