d9e4e35674d1e6379b52ab01fe8d2c718d6d5f8e
Petr Sahula <spetex@icloud.com>
2015-10-13 12:36:25 +0200

    Fix filepaths for docs-updater.sh

--- docs-updater.sh
+++ docs-updater.sh
@@ -1,6 +1,5 @@
-cd pages/docs/
+cd /webapps/CoopDocs/pages/docs/
 git pull
 ./indexgen.sh
-cd ..
-cd ..
+cd /webapps/CoopDocs/
 gatsby build && echo "Gatsby has builded your documentation. Rejoice!"
