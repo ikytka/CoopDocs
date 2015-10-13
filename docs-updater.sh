c60b55ff7d08f0188e625af12cd2886c97631d4b
Petr Sahula <spetex@icloud.com>
2015-10-13 16:32:11 +0200

    Escape the exclamation mark
    Because I have been debugging this for almost 2 hours.

--- docs-updater.sh
+++ docs-updater.sh
@@ -3,4 +3,4 @@ git pull && echo "Pulled new version from the repo successful."
 ./indexgen.sh && echo "Indexgen script finished building CoopDocs Index File."
 cd /webapps/CoopDocs/
 echo "Trying to build CoopDocs."
-gatsby build && echo "Gatsby has builded your documentation. Rejoice!"
+gatsby build && echo "Gatsby has builded your documentation. Rejoice\!"
