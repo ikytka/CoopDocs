496620e654a06e72205bda1b6009322565bbda9c
Petr Sahula <spetex@icloud.com>
2015-10-14 15:45:12 +0200

    Cron job finally fixed!
    There was a wrong path to gatsby module.

--- docs-updater.sh
+++ docs-updater.sh
@@ -1,7 +1,6 @@
-PATH=$PATH"/usr/local/bin/"
 cd /webapps/CoopDocs/pages/docs/
 git pull && echo "Pulled new version from the repo successful."
 ./indexgen.sh && echo "Indexgen script finished building CoopDocs Index File."
 cd /webapps/CoopDocs/
 echo "Trying to build CoopDocs."
-gatsby build && echo "Gatsby has builded your documentation. Rejoice."
+usr/local/bin/gatsby build && echo "Gatsby has builded your documentation. Rejoice."
